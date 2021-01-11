8f15c23a060d5dd47d97dc9865945d9a14c72b45
mattmaniak <72657016+mattmaniak@users.noreply.github.com>
2021-01-11 10:40:11 +0100

    Dump pipes to graveyard

--- Assets/Scripts/PipesRenderer.cs
+++ Assets/Scripts/PipesRenderer.cs
@@ -1,7 +1,32 @@
+using System;
 using UnityEngine;
 
 public class PipesRenderer : MonoBehaviour
 {
+    readonly Vector3 graveyardPosition = new Vector3(-100.0f, 0.0f, 0.0f);
+
+    float minHorizontalSeparationOffset = 5.0f;
+    float maxHorizontalSeparationOffset = 10.0f;
+    int currentSectionIndex = 0;
+
     [SerializeField]
-    GameObject[] pipes;
+    GameObject[] sections;
+
+    void Awake()
+    {
+        foreach (var section in sections)
+        {
+            section.transform.position = graveyardPosition;
+            section.SetActive(true);
+        }
+    }
+
+    [Obsolete("Temponary and CPU-heavy solution. Use events in production.")]
+    void Update()
+    {
+        if (!sections[currentSectionIndex].GetComponentInChildren<Renderer>().isVisible)
+        {
+
+        }
+    }
 }
