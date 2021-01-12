f0a34fa2a832b9b33b306e2957420535a1772326
mattmaniak <72657016+mattmaniak@users.noreply.github.com>
2021-01-12 12:05:19 +0100

    Add physics - falling and altitude gaining

--- Assets/Scripts/Player/Controls.cs
+++ Assets/Scripts/Player/Controls.cs
@@ -3,8 +3,29 @@ using UnityEngine;
 
 namespace Player
 {
+    [RequireComponent(typeof(Movement))]
+    [RequireComponent(typeof(Rigidbody2D))]
     public class Controls : MonoBehaviour
     {
+        Rigidbody2D rigidbody;
+
+        void Awake()
+        {
+            rigidbody = GetComponent<Rigidbody2D>();
+        }
+
+        void FixedUpdate()
+        {
+            if (rigidbody.velocity.y > Movement.maxVelocity)
+            {
+                rigidbody.velocity = new Vector2(0.0f, Movement.maxVelocity);
+            }
+            else if (rigidbody.velocity.y < -Movement.maxVelocity)
+            {
+                rigidbody.velocity = new Vector2(0.0f, -Movement.maxVelocity);
+            }
+        }
+
         /// <remarks>
         /// Not the best solution. Events would be strongly preferred, however
         /// probably there is not a solution in unity that implements it for
@@ -14,21 +35,22 @@ namespace Player
         void Update()
         {
 #if UNITY_EDITOR
-            HandleMouseInput();
+            HandleKeyboardInput();
 #else
             HandleTouchInput();
 #endif
         }
 
-        void HandleMouseInput()
+        void GainAltitude()
+        {
+            rigidbody.AddForce(Vector2.up * Movement.maxVelocity * 2.0f);
+        }
+
+        void HandleKeyboardInput()
         {
             if (Input.GetKey(KeyCode.Space))
             {
-                throw new NotImplementedException();
-            }
-            else if (Input.GetKeyUp(KeyCode.Space))
-            {
-                throw new NotImplementedException();
+                GainAltitude();
             }
         }
 
@@ -39,11 +61,10 @@ namespace Player
                 switch (Input.GetTouch(0).phase)
                 {
                     case TouchPhase.Began:
-                        throw new NotImplementedException();
-                
+                        GainAltitude();
+                        break;
+
                     case TouchPhase.Ended:
-                        throw new NotImplementedException();
-    
                     default:
                         break;
                 }
