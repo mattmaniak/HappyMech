f963d2c9b1513ba80cc1694ef72a2b4e8b8680c2
mattmaniak <72657016+mattmaniak@users.noreply.github.com>
2021-01-14 14:36:34 +0100

    Fix IAPButton problems, add really fake purchase on Android

--- Assets/Scripts/GUI/GameOverMenuController.cs
+++ Assets/Scripts/GUI/GameOverMenuController.cs
@@ -4,21 +4,6 @@ namespace GUI
 {
     public class GameOverMenuController : AbstractMenuController
     {
-        [SerializeField]
-        GameObject androidFakePurchaseText;
-
-        [SerializeField]
-        GameObject iapFakePurchaseText;
-
-        void Awake()
-        {
-#if UNITY_EDITOR
-            androidFakePurchaseText.SetActive(false);
-#else
-            unityEditorFakePurchaseText.SetActive(false);
-#endif
-        }
-
         public override void PlayLevel()
         {
 #if !UNITY_EDITOR
